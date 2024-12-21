fbae531421f89ec9bff07776136f5835d282b3cf
Leonardo Anjos <leodrums11@gmail.com>
2024-12-20 23:51:00 -0300

    load default config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { SalesModule } from './sales/sales.module';
 
 @Module({
   imports: [ProductsModule, SalesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
