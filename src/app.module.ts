827563d0ae5e7ac778a94c9fd3dbeb1c361a634f
Leonardo Anjos <leodrums11@gmail.com>
2024-12-20 23:54:28 -0300

    add new external service - whatsppa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { SalesModule } from './sales/sales.module';
+import { WhatsappService } from './whatsapp/whatsapp.service';
 
 @Module({
   imports: [ProductsModule, SalesModule],
+  providers: [WhatsappService],
 })
 export class AppModule {}
