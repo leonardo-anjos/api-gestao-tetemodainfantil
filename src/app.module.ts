205d882dcc70813b8b7876688b8b06f3c67f5e9b
Leonardo Anjos <leodrums11@gmail.com>
2024-12-20 23:57:24 -0300

    add database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { SalesModule } from './sales/sales.module';
 import { WhatsappService } from './whatsapp/whatsapp.service';
 
 @Module({
-  imports: [ProductsModule, SalesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/api-gestao-tetemodainfantil'),
+    ProductsModule,
+    SalesModule,
+  ],
   providers: [WhatsappService],
 })
 export class AppModule {}
