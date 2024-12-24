76e393504500999ed71581f0dedbe0386e2fa58b
Leonardo Anjos <leodrums11@gmail.com>
2024-12-24 01:25:36 -0300

    enable cors and set new port

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-   app.use(express.json({ limit: '50mb' }));
-   app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.enableCors(); 
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
